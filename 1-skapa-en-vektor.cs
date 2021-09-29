1c4225472ac315f49c042fa499ec12c4d4cf2cf2
Alexander <a.wiklow@gmail.com>
2021-09-29 17:00:24 +0200

    Create a vector

---
+++ 1-skapa-en-vektor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_skapa_en_vektor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapar en vektor med plats för 10 stycken tal
+            int[] temp = new int[10];
+
+        }
+    }
+}
